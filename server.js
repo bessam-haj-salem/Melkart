413114db1a49d380c2f21851bf7ba646bceb70f3
bessam-haj-salem <bessamhajsalem@gmail.com>
2020-08-23 18:18:31 +0100

    initial heroku

--- server.js
+++ server.js
@@ -5,14 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/MELKART'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/MELKART/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
-
-console.log('console listening')
